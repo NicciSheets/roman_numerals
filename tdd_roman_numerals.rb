9d2e3bcbbcfc3ac561d2e52e0423fdcac4440044
Nicci Sheets <niccisheets@aol.com>
2018-10-15 10:26:46 -0400

    failed test for result is a string

--- tdd_roman_numerals.rb
+++ tdd_roman_numerals.rb
@@ -1,9 +1,13 @@
 require "minitest/autorun"
-require_relative "tdd_roman_numerals.rb"
+require_relative "roman_numerals.rb"
 
 class TddRomanNumerals < Minitest::Test
 
 	def test_assert_1_equals_1
 		assert_equal(1, 1)
+	end
+
+	def test_that_result_is_string
+		assert_equal(String, 1.romanizer().class)
 	end
 end
