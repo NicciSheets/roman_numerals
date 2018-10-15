49f76666c96a771fa8e091eff0ef55149105b4de
Nicci Sheets <niccisheets@aol.com>
2018-10-15 15:45:24 -0400

    oops this is first commit for real

--- de_roman_numerals.rb
+++ de_roman_numerals.rb
@@ -1,9 +1,5 @@
 class String
 
-	def test_assert_1_equals_1
-		assert_equal(1, 1)
-	end
 
-	
 
 end
--- tdd_de_roman_numerals.rb
+++ tdd_de_roman_numerals.rb
@@ -3,4 +3,8 @@ require_relative "de_roman_numerals.rb"
 
 class TddDeRomanNumerals < Minitest::Test
 
+	def test_assert_1_equals_1
+		assert_equal(1, 1)
+	end
+
 end
