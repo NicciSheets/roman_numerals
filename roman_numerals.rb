e4ded785f039d48df5191909e1c13f43b5e563be
Nicci Sheets <niccisheets@aol.com>
2018-10-15 11:13:30 -0400

    passed test for 4 is iv

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -2,12 +2,15 @@ class Integer
 
 	def romanizer()
 		numerals = {
+			"IV" => 4,
 			"I" => 1
 		}
 		x = self
 		roman = ""
 		numerals.each do |key, value|
-			roman << key * x
+			roman << key * (x/value)
+			x %= value
+
 		end
 		roman
 	end
