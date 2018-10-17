468d988ecfddecfc4d15fa03fa879b518145c9b1
Nicci Sheets <niccisheets@aol.com>
2018-10-17 13:39:32 -0400

    passed and failed for 4

--- de_roman_numerals.rb
+++ de_roman_numerals.rb
@@ -2,12 +2,15 @@ class String
 
 	def deromanizer()
 		arabic = {
+			"IV" => 4,
 			"I" => 1
 		}
 		result = 0
 		str = self
 		arabic.each do |key, value|
 			result += value * (str.scan(/#{key}/).count)
+			temp = [key]
+            temp.each {|v| str.sub!(v, '')}
 		end
 		result
 	end
